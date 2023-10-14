6c2aee6b46a7eebcad1f830aa9a364c0cc6746d6
saiharihar <103887110+saiharihar@users.noreply.github.com>
2023-10-14 16:46:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [dob, setDob] = useState(null)
+  const [age, setAge] = useState(0);
+
+  const handleClick = () => {
+    var curr_year = parseInt(new Date().getUTCFullYear())
+    var curr_month = parseInt(new Date().getMonth());
+
+    var year =  parseInt(String(dob).split('-')[0]);
+    var month = parseInt(String(dob).split('-')[1]);
+    
+    var diff = (curr_month - month) + ((curr_year - year) * 12)
+  
+    // var tage =  Math.abs(age_dt.getUTCFullYear() - 1970);
+    setAge(Math.floor(diff / 12));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Age Calculator</h1>
+      <h4>Enter your date of birth</h4>
+      <input onChange={(e) => setDob(e.target.value)} type="date" name="dob" id="dob" />
+      <button onClick={handleClick}>Calculate Age</button>
+      <h3>You are {age} years old</h3>
     </div>
   );
 }
